bdb3dd0d05115fd4eed3bcd88dac3dd085ded0ce
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-09-21 21:21:00 +0300

    Add Room DTOs and update project structure
    Removed the unused `Class1` class from `HotelProject.DtoLayer`.
    Added `RoomAddDto` and `UpdateRoomDto` classes to handle room creation and updates, including validation attributes for required fields and constraints.
    Updated `HotelProject.DtoLayer.csproj` to include new folders for organizing DTOs: `Service`, `Subscribe`, `Staff`, and `Testimonial`.

---
+++ ApiConsume/HotelProject.DtoLayer/Dtos/Room/RoomAddDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.DtoLayer.Dtos.Room;
+
+public sealed class RoomAddDto
+{
+    [Required(ErrorMessage = "Please Enter a Room Number")]
+    public string RoomNumber { get; set; } = default!;
+    public string CoverImage { get; set; }
+    [Required(ErrorMessage = "Please Enter a Price")]
+    public float Price { get; set; }
+    [Required(ErrorMessage = "Please Enter a Title")]
+    public string Title { get; set; } = default!;
+    [Required(ErrorMessage = "Please Enter a Bed Count")]
+    public int BedCount { get; set; }
+    [Required(ErrorMessage = "Please Enter a Bath Count")]
+    public int BathCount { get; set; }
+    [Required(ErrorMessage = "Please Enter a Room Description")]
+    public string Description { get; set; } = default!;
+}
---
+++ ApiConsume/HotelProject.DtoLayer/Dtos/Room/UpdateRoomDto.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.DtoLayer.Dtos.Room;
+
+public sealed class UpdateRoomDto
+{
+    public int RoomId { get; set; }
+
+    [Required(ErrorMessage = "Please Enter a Room Number")]
+    public string RoomNumber { get; set; } = default!;
+
+    [Required(ErrorMessage = "Please Enter a Cover Image URL")]
+    public string CoverImage { get; set; }
+
+    [Required(ErrorMessage = "Please Enter a Price")]
+    [MaxLength(5000, ErrorMessage = "Price cannot exceed 5000")]
+    public float Price { get; set; }
+
+    [Required(ErrorMessage = "Please Enter a Title")]
+    [MaxLength(100, ErrorMessage = "Title cannot exceed 100 characters")]
+    public string Title { get; set; } = default!;
+
+    [Required(ErrorMessage = "Please Enter a Bed Count")]
+    [MaxLength(10, ErrorMessage = "Bed Count cannot exceed 10")]
+    public int BedCount { get; set; }
+
+    [Required(ErrorMessage = "Please Enter a Bath Count")]
+    [MaxLength(10, ErrorMessage = "Bath Count cannot exceed 10")]
+    public int BathCount { get; set; }
+
+    [Required(ErrorMessage = "Please Enter a Room Description")]
+    [MaxLength(1000, ErrorMessage = "Description cannot exceed 1000 characters")]
+    public string Description { get; set; } = default!;
+}
