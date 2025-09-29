604a628ef2381dd22fec52a82d0bfc55eef930cb
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-09-29 23:06:00 +0300

    add guest dto's

---
+++ Frontend/HotelProject.WebUI/Dtos/GuestDto/ResultGuestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelProject.WebUI.Dtos.GuestDto;
+
+public class ResultGuestDto
+{
+    public int GueastId { get; set; }
+    public string Name { get; set; } = default!;
+    public string Surname { get; set; } = default!;
+    public string City { get; set; } = default!;
+}
---
+++ Frontend/HotelProject.WebUI/Dtos/GuestDto/UpdateGuestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelProject.WebUI.Dtos.GuestDto;  
+
+public class UpdateGuestDto
+{
+    public int GueastId { get; set; }
+    public string Name { get; set; } = default!;
+    public string Surname { get; set; } = default!;
+    public string City { get; set; } = default!;
+}
